068e148fb400b039d1779cf5ffd29ef0202b9807
Baozi Wu <baojjeu@gmail.com>
2014-12-01 11:29:15 +0800

    Implement sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
   root 'projects#index'
 
+  resources :users
   resources :projects do
     resources :tickets
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe User, :type => :model do
   describe 'passwords' do
     it 'needs a password and confirmation to save' do
