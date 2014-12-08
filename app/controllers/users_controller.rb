ee075ef79bbbc839a1f523e81537eaa0623dd106
Baozi Wu <baojjeu@gmail.com>
2014-12-08 12:58:03 +0800

    Don't show projects that a user doesn't have permission to see

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to projects_path, flash: { success: 'You have signed up successfully.' }
+      session[:user_id] = @user.id
+      redirect_to projects_path,
+        flash: { success: 'You have signed up successfully.' }
     else
       render :new
     end
