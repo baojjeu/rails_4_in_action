120a92336ef387540aaae1463da722c55c55da1d
Baozi Wu <baojjeu@gmail.com>
2014-12-12 19:58:16 +0800

    Add a seeds file

---
+++ spec/features/seeds_spec.rb
@@ -0,0 +1,7 @@
+feature 'Seed data' do
+  scenario 'The basics' do
+    load "#{Rails.root}/db/seeds.rb"
+    user = User.where(email: "admin@example.com").first!
+    project = Project.where(name: 'Ticketee Beta').first!
+  end
+end
