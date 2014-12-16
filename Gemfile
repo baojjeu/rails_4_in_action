f85f6c7b0d8d6d546f5ec816933a55af1f6dc735
Baozi Wu <baojjeu@gmail.com>
2014-12-16 21:48:31 +0800

    Provide an 'Add another file' link that ises javascript so that users can upload more than one file

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'jquery-turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
@@ -15,6 +16,7 @@ gem 'bcrypt-ruby'
 gem 'bootstrap-sass'
 gem 'cancancan', '~> 1.9'
 gem 'carrierwave'
+gem 'database_cleaner'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.1.0'
@@ -24,4 +26,5 @@ end
 group :test do
   gem 'capybara', '2.4.4'
   gem 'factory_girl_rails', '4.5.0'
+  gem 'selenium-webdriver', '2.44.0'
 end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -27,13 +27,14 @@ feature 'Creating Tickets' do
     end
   end
 
-  scenario 'Creating a ticket with an attachment' do
+  scenario 'Creating a ticket with an attachment', js: true do
     fill_in "Title", with: "Add documentation for blink tag"
     fill_in "Description", with: "The blink tag has a speed attribute"
 
-    attach_file 'File #1', 'spec/fixtures/speed.txt'
-    attach_file 'File #2', 'spec/fixtures/speed2.txt'
-    attach_file 'File #3', 'spec/fixtures/speed3.txt'
+    attach_file 'File #1', "#{Rails.root}/spec/fixtures/speed.txt"
+
+    click_link 'Add another file'
+    attach_file 'File #2', "#{Rails.root}/spec/fixtures/speed2.txt"
 
     click_button 'Create Ticket'
 
@@ -42,7 +43,6 @@ feature 'Creating Tickets' do
     within('#ticket .assets') do
       expect(page).to have_content('speed.txt')
       expect(page).to have_content('speed2.txt')
-      expect(page).to have_content('speed3.txt')
     end
   end
 
@@ -52,5 +52,14 @@ feature 'Creating Tickets' do
     expect(page).to have_content('Ticket has not been created.')
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank")
+  end
+
+  scenario 'Description must be longer than 10 characters' do
+    fill_in 'Title', with: 'hello'
+    fill_in 'Description', with: '123'
+    click_button 'Create Ticket'
+
+    expect(page).to have_content('Ticket has not been created.')
+    expect(page).to have_content('Description is too short')
   end
 end
