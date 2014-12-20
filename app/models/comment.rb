8ddcd4025bb16afdb6e7f6081a0dffff2cf51580
Baozi Wu <baojjeu@gmail.com>
2014-12-20 21:32:31 +0800

    Display a comment's state transition.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   validates :text, presence: true
@@ -8,10 +9,15 @@ class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :user
   belongs_to :state
+  belongs_to :previous_state, class_name: 'State'
 
   private
     def set_ticket_state
       self.ticket.state = self.state
       self.ticket.save!
+    end
+
+    def set_previous_state
+      self.previous_state = ticket.state
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217143819) do
+ActiveRecord::Schema.define(version: 20141220124617) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141217143819) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "state_id"
+    t.integer  "previous_state_id"
   end
 
   add_index "comments", ["ticket_id"], name: "index_comments_on_ticket_id"
